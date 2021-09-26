9c81a9b315bb12c41645976ada388b7d18497df2
Peng Xiao <584378+pengx17@users.noreply.github.com>
2021-09-26 10:05:34 +0800

    fix: prepare for publish to marketplace

---
+++ release.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    [
+      "@semantic-release/npm",
+      {
+        npmPublish: false,
+      },
+    ],
+    "@semantic-release/git",
+    [
+      "@semantic-release/exec",
+      {
+        prepareCmd:
+          "zip -qq -r logseq-laurel-theme-${nextRelease.version}.zip laurel.png *.css readme.md LICENSE package.json",
+      },
+    ],
+    [
+      "@semantic-release/github",
+      {
+        assets: "logseq-laurel-theme-*.zip",
+      },
+    ],
+  ],
+};
